de8982c99f7dee99b157388b4dda965c7a211415
Sven Kaack <sven.kaack@gmx.de>
2025-06-08 15:50:02 +0200

    final updates in db/index.js and root index.js

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    const client = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected", client.connection.host);
 } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import express from "express";
 import "./db/index.js";
+import errorHandler from "./middleware/errorHandler.js";
+import bookRouter from "./routers/bookRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use("/books", bookRouter);
+app.use("/users", userRouter);
+
 app.use("*splat", (req, res) => {
     throw new Error("Not found", { cause: 404 });
 });
 
+app.use(errorHandler);
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
